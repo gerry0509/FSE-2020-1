52d1e99e1a680fdc22e1eb3bdf6f18a6c0f7ad94
gerry0509 <39445714+gerry0509@users.noreply.github.com>
2019-09-09 18:08:03 -0500

    Add files via upload

---
+++ Practicas_Raspberry/Practica2/Ejercicio1/exit_asm.s
@@ -0,0 +1,15 @@
+@ Syscall defines
+.equ SYSCALL_EXIT,     1
+
+
+        .globl _start
+_start:
+
+        bl exit
+        
+exit:
+
+	@ YOUR CODE HERE
+	mov r0,#42
+	mov r7,#SYSCALL_EXIT
+	swi 0x0
